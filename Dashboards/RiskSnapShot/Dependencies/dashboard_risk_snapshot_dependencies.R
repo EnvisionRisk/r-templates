77b13c7b613e5663cef61e68ee8165bf804e7228
Jonas Hal <jonas.hal@envisionrisk.com>
2022-11-28 15:36:58 +0100

    reference bug

--- Dashboards/RiskSnapShot/Dependencies/dashboard_risk_snapshot_dependencies.R
+++ Dashboards/RiskSnapShot/Dependencies/dashboard_risk_snapshot_dependencies.R
@@ -106,8 +106,8 @@ dashboard_risk_snapshot <- function(access_token, date, positions, risk_measure,
   }
   random_id <- IdGenerator(1)
   output_file_name <- paste0("Report_Risk_Snapshot_", out_workflow_risk_snapshot$Input$date, "_id_", random_id)
-  suppressWarnings(rmarkdown::render(input       = paste0(getwd(), "/Dashboards/RiskSnapShot/dashboard_risk_snapshot.Rmd"),
-                                     output_file = paste0(getwd(), "/Output/", output_file_name, ".html"),
+  suppressWarnings(rmarkdown::render(input       = paste0(getwd(), "/Dashboards/RiskSnapShot/Dependencies/dashboard_risk_snapshot.Rmd"),
+                                     output_file = paste0("/home/rstudio/r-templates-main/Dashboards/RiskSnapShot/Output", output_file_name, ".html"),
                                      params      = list(workflow_object = out_workflow_risk_snapshot),
                                      quiet       = TRUE))
 
