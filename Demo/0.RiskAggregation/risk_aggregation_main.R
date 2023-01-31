755f31c7f40b6267a563ff7f1aa7a8e1d5af2c26
Jonas Hal <jonas.hal@envisionrisk.com>
2023-01-31 19:44:26 +0100

    changed aggregation demo

--- Demo/0.RiskAggregation/risk_aggregation_main.R
+++ Demo/0.RiskAggregation/risk_aggregation_main.R
@@ -10,11 +10,7 @@ source("Demo/0.RiskAggregation/Dependencies/risk_aggregation_dependencies.R")
 Sys.setenv("USR_ID"  = getPass::getPass(msg = "Please provide email: ", noblank = TRUE, forcemask = FALSE))
 Sys.setenv("USR_PWD" = getPass::getPass(msg = "Please provide password: ", noblank = TRUE, forcemask = FALSE))
 
-#******************************************************************************
-#*
-#* AUTHENTICATIO WITH THE RISK SERVER 
-#*
-#******************************************************************************
+#### AUTHENTICATIO WITH THE RISK SERVER ####
 # Retrieve the access-token from the Auth-server.
 my_access_token <- get_access_token(Sys.getenv("USR_ID"), 
                                     Sys.getenv("USR_PWD"))
@@ -26,10 +22,8 @@ access_token    <- my_access_token[["access-token"]]
 #The access-token is valid until;
 print(paste0("The access-token is valid until: ", my_access_token[["access-token-expiry"]]))
 
-#******************************************************************************
-#*
-#* DEMONSTRATION
-#*
+
+#### DEMONSTRATION ####
 #******************************************************************************
 # The purpose of the demonstration is to illustrate how to decorate a 
 # portfolio of financial positions with risk predictions using EnvisionRisk’s 
@@ -42,47 +36,23 @@ print(paste0("The access-token is valid until: ", my_access_token[["access-token
 #
 # The risk is calculated using the endpoint:
 # 'https://api.envisionrisk.com/v1/themis/portfolio-risk-component' 
-#******************************************************************************
-
-#******************************************************************************
-#*
-#### INPUT for the demonstration - portfolio positions, date and volatility_id
-#*
-##******************************************************************************
-# Portfolio positions (predefined example portfolio is available in the '/Data' folder)
-demo_port_data <- base::readRDS("Data/example_port_structure.rds")
-
-#### Alternatively download the portfolio-file used for the demonstration from an external location ####
-#demo_port <- base::readRDS(base::url("https://www.dropbox.com/s/h54j08xb5envg4p/example_port_structure.rds?raw=1"))
-#base::closeAllConnections()
-
-# In case you want to get a list of available instruments (symbols) to use for your own portfolio construction:
-# dt_available_instrumements <- envrsk_instrument_search(access_token = access_token, valid_at = Sys.Date())
-# dt_available_instrumements
-#demo_port_data <- data.table("symbol" = sample(dt_available_instrumements[instrument_type == "single_stock", symbol], 1000))
-demo_port_data[, ":=" (position_type = "single_stock",
-                       quantity      = 1,
-                       location      = "Equity")]
-
-#******************************************************************************
-#*
-#### DEMONSTRATION - user defined portfolio structure - unconditional risk ####
-#*
-#******************************************************************************
+#
 # Call the EnvisionRisk API. The API endpoint are called through the R-specific 
 # function 'envrsk_portfolio_risk_regular'. This function takes care of making 
 # the input data ready for the API request and formatting the response from the 
 # API into R-specific data structures. The output from the API is saved into the
 # variable 'demo_port_risk_out'.
+#******************************************************************************
 
-#******************************************************************************
-#*
-#### Value-at-Risk (VaR) & Expected-Shortfall (ES) ####
-#*
-#******************************************************************************
+#### SETTINGS ####
 demo_cur  <- "DKK"
-demo_date <- as.Date("2022-12-28")
+demo_date <- as.Date("2023-01-01")
 
+#### IMPORT PORTFOLIO ####
+# Portfolio positions (predefined example portfolio is available in the '/Data' folder)
+demo_port_data <- base::readRDS("Data/treasury_example_port.rds")
+
+#### API REQUEST ####
 # Calculate Risk as point-in-time, 1-day 97.5% Expected-shortfall
 demo_port_risk_out <- envrsk_portfolio_risk_regular(
     access_token  = access_token, 
@@ -119,9 +89,11 @@ demo_port_perf_out <- envrsk_portfolio_hypothetical_performance(
   date          = demo_date,
   base_cur      = demo_cur)
 
+
 # Retain the data we need from API call.
 dt_demo_port_perf <- format_portfolio_perf(demo_port_perf_out)
 
+#### CONSOLIDATE THE API RESPONSE ####
 # Merge the data from the 3 API calls
 dt_demo_port <- merge(
   merge(dt_demo_port_risk[,.(pathString, position_type, label, quantity, "Value-at-Risk" = VaR, "Expected-Shortfall" = ES)],
@@ -130,6 +102,7 @@ dt_demo_port <- merge(
   dt_demo_port_perf[,.(pathString, Notational, MarketValue, HypotheticalPnL)],
   by = "pathString")
 
+#### FORMAT THE RESPONSE ####
 # Transform the portfolio to a data.tree object (more pleasing for the eye)
 demo_port <- data.tree::as.Node(dt_demo_port)
 
@@ -145,21 +118,16 @@ data.tree::SetFormat(demo_port, "Value-at-Risk",  formatFun = function(x) format
 data.tree::SetFormat(demo_port, "Expected-Shortfall",  formatFun = function(x) format(round(as.numeric(x), 0), nsmall=0, big.mark=","))
 data.tree::SetFormat(demo_port, "Stress-Test",       formatFun = function(x) format(round(as.numeric(x), 0), nsmall=0, big.mark=","))
 
+
+#### PRESENT THE RESULT ####
 print(demo_port, 
       "Position Type"   = "position_type", 
       "Instrument Name" = "label", 
       "Quantity"        = "quantity",
-      "Market Value" = "MarketValue",
-      "Risk" = "Expected-Shortfall",
-      "Stress" = "Stress-Test")
+      "Market Value"    = "MarketValue",
+      "Risk"            = "Expected-Shortfall",
+      "Stress"          = "Stress-Test")
 
-# print(demo_port, 
-#       "Position Type"   = "position_type", 
-#       "Instrument Name" = "label", 
-#       "Quantity"        = "quantity",
-#       "Value-at-Risk",
-#       "Expected-Shortfall",
-#       "Stress-Test")
 
 
 
