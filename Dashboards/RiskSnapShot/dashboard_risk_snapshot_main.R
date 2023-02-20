4884755940cf8e3c5d2b82b8ceb3b9962cd06faa
Jonas Hal <jonas.hal@envisionrisk.com>
2023-02-20 16:42:41 +0100

    changed the auth flow for the risksnapshot dashboard

--- Dashboards/RiskSnapShot/dashboard_risk_snapshot_main.R
+++ Dashboards/RiskSnapShot/dashboard_risk_snapshot_main.R
@@ -4,33 +4,10 @@ options(scipen=999)
 source("envrsk_api_bridge_2_R.R")
 source("Dashboards/RiskSnapShot/Dependencies/dashboard_risk_snapshot_dependencies.R")
 
-#### CREDENTIALS ####
-# Provide credentials - email and password. In case you have not yet received 
-# your personal credentials, contact EnvisionRisk at info@envisionrisk.com
-Sys.setenv("USR_ID"  = getPass::getPass(msg = "Please provide email: ", noblank = TRUE, forcemask = FALSE))
-Sys.setenv("USR_PWD" = getPass::getPass(msg = "Please provide password: ", noblank = TRUE, forcemask = FALSE))
+#### AUTHENTICATE ####
+envrsk_auth_renew_access_token()
+access_token <- my_access_token[["access-token"]]
 
-#******************************************************************************
-#*
-#* AUTHENTICATIO WITH THE RISK SERVER 
-#*
-#******************************************************************************
-# Retrieve the access-token from the Auth-server.
-my_access_token <- get_access_token(Sys.getenv("USR_ID"), 
-                                    Sys.getenv("USR_PWD"))
-
-# Extract the access-token. The access-token is valid for 24 hours after
-# it has been requested. 
-access_token    <- my_access_token[["access-token"]]
-
-#The access-token is valid until;
-print(paste0("The access-token is valid until: ", my_access_token[["access-token-expiry"]]))
-
-#******************************************************************************
-#*
-#### INPUT for the demonstration - date, volatility_id and portfolio positions
-#*
-##******************************************************************************
 # Portfolio positions (predefined example portfolio is available in the 
 # '/Data' folder)
 demo_port <- base::readRDS("Data/example_port_structure.rds")
