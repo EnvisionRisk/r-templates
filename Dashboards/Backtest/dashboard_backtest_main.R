169a6c008c7ce6de8f05e97d0a7f4f9da88843b7
Jonas Hal <jonas.hal@envisionrisk.com>
2023-02-20 16:58:31 +0100

    changed the auth flow

--- Dashboards/Backtest/dashboard_backtest_main.R
+++ Dashboards/Backtest/dashboard_backtest_main.R
@@ -4,27 +4,9 @@ options(scipen=999)
 source("envrsk_api_bridge_2_R.R")
 source("Dashboards/Backtest/Dependencies/dashboard_backtest_dependencies.R")
 
-#### CREDENTIALS ####
-# Provide credentials - email and password. In case you have not yet received 
-# your personal credentials, contact EnvisionRisk at info@envisionrisk.com
-Sys.setenv("USR_ID"  = getPass::getPass(msg = "Please provide email: ", noblank = TRUE, forcemask = FALSE))
-Sys.setenv("USR_PWD" = getPass::getPass(msg = "Please provide password: ", noblank = TRUE, forcemask = FALSE))
-
-#******************************************************************************
-#*
-#* AUTHENTICATIO WITH THE RISK SERVER 
-#*
-#******************************************************************************
-# Retrieve the access-token from the Auth-server.
-my_access_token <- get_access_token(Sys.getenv("USR_ID"), 
-                                    Sys.getenv("USR_PWD"))
-
-# Extract the access-token. The access-token is valid for 24 hours after
-# it has been requested. 
-access_token    <- my_access_token[["access-token"]]
-
-#The access-token is valid until;
-print(paste0("The access-token is valid until: ", my_access_token[["access-token-expiry"]]))
+#### AUTHENTICATE ####
+envrsk_auth_renew_access_token()
+access_token <- my_access_token[["access-token"]]
 
 #******************************************************************************
 # Portfolio backtest (Th example backtest data is available in the '/Data' folder)
